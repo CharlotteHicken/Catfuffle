05740d177ffb52ef86514e9bd678f02880994f50
CharlotteHicken <hickecha@sheridancollege.ca>
2025-01-29 14:12:26 -0500

    Jump and physics movement modified into script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rb;
+
+    [Header("Movement Settings")]
+    public float maxSpeed = 5;
+    public float accelerateTime = 0.2f;
+    public float decelerateTime = 0.2f;
     float acceleration;
     float deceleration;
-    public float maxSpeed = 5;
-    public float accelerateTime = 1;
-    public float decelerateTime = 1;
+
+    [Header("Jump Settings")]
+    float gravity;
+    float initialJumpSpeed;
+    public float apexHeight = 3f;
+    public float apexTime = 0.5f;
+    public float maxVelocity = 15f;
+
+    [Header("Ground Check Settings")]
+    bool isGrounded = false;
+    public float groundCheckOffset = 0.5f;
+    public Vector3 groundCheckSize = new(0.4f, 0.1f);
+    public LayerMask groundCheckMask;
+
     Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        //rb.constraints = RigidbodyConstraints.FreezeRotation; // Prevent rolling but also stops movement
+
+        gravity = -2 * apexHeight / (Mathf.Pow(apexTime, 2));
+        initialJumpSpeed = 2 * apexHeight / apexTime;
+
         acceleration = maxSpeed / accelerateTime;
         deceleration = maxSpeed / decelerateTime;
     }
@@ -21,26 +45,63 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float leftRightInput = Input.GetAxis("Horizontal");
-        float upDownInput = Input.GetAxis("Vertical");
-        Vector3 inputValues = new Vector3(leftRightInput, 0, upDownInput);
+        CheckForGround();
+    }
 
-        if (inputValues.magnitude != 0)
+    private void FixedUpdate()
+    {
+        Vector3 playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        MovementUpdate(playerInput);
+
+        JumpUpdate();
+
+        rb.velocity = new Vector3(velocity.x, velocity.y, velocity.z);
+    }
+
+    private void MovementUpdate(Vector3 playerInput)
+    {
+        velocity.x = CalculateMovementInput(playerInput.x, velocity.x);
+        velocity.z = CalculateMovementInput(playerInput.z, velocity.z);
+    }
+
+    private float CalculateMovementInput(float input, float velocity)
+    {
+        if (input != 0)
         {
-            velocity += inputValues * acceleration * Time.deltaTime;
+            velocity += acceleration * input * Time.fixedDeltaTime;
+            velocity = Mathf.Clamp(velocity, -maxSpeed, maxSpeed);
         }
         else
         {
-            velocity -= velocity * deceleration * Time.deltaTime;
+            velocity = Mathf.MoveTowards(velocity, 0, deceleration * Time.fixedDeltaTime);
         }
+        return velocity;
+    }
 
-        if (velocity.magnitude < 0.001f)
+    private void JumpUpdate()
+    {
+        if (!isGrounded)
         {
-            velocity = Vector3.zero;
+            velocity.y += gravity * Time.fixedDeltaTime;
+        }
+        else
+        {
+            velocity.y = -0.1f;
         }
 
-        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        if (isGrounded && Input.GetButton("Jump"))
+        {
+            velocity.y = initialJumpSpeed;
+            isGrounded = false;
+        }
 
-        transform.position += velocity * Time.deltaTime;
+        // Clamp falling speed to terminal velocity
+        velocity.y = Mathf.Max(velocity.y, -maxVelocity);
+    }
+
+    private void CheckForGround()
+    {
+        isGrounded = Physics.CheckBox(transform.position + Vector3.down * groundCheckOffset, groundCheckSize / 2, Quaternion.identity, groundCheckMask.value); //if physics box collides with the ground, player is grounded
     }
 }
