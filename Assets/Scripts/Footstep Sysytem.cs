104ab385cd5a6e78c1592fd017e7eabcd7ca3f41
MrBigBrain0 <sicilisi@sheridancollege.ca>
2025-03-25 17:24:04 -0400

    SFX
    - footstep sounds work and are synced im finished

--- Assets/Scripts/Footstep Sysytem.cs
+++ Assets/Scripts/Footstep Sysytem.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class FootstepSoundManager : MonoBehaviour
 {
-    public AudioClip footstepSound; 
+    public AudioClip footstepSoundR;
+    public AudioClip footstepSoundL;
     private AudioSource audioSource;
 
     void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.clip = footstepSound;
+        audioSource.clip = footstepSoundL;
         audioSource.playOnAwake = false; 
     }
 
-    public void PlayFootstepSound()
+    public void PlayFootstepSoundL()
     {
         Debug.Log("hi");
-        audioSource.PlayOneShot(footstepSound);
+        audioSource.PlayOneShot(footstepSoundL);
+    }
+
+    public void PlayFootstepSoundR()
+    {
+        audioSource.PlayOneShot(footstepSoundR);
     }
 }
